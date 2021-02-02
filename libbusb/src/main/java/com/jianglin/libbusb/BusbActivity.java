97c909580865c429e4b70bd4c8272d1bd773f9f5
jiang <jianglinair@163.com>
2021-02-02 14:04:21 +0800

    modb

--- libbusb/src/main/java/com/jianglin/libbusb/BusbActivity.java
+++ libbusb/src/main/java/com/jianglin/libbusb/BusbActivity.java
@@ -3,20 +3,25 @@ package com.jianglin.libbusb;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.jianglin.common.BaseActivity;
 import com.jianglin.libexpa.BusaPath;
 import com.jianglin.libexpb.BusbPath;
 
 @Route(path = BusbPath.BUSB)
-public class BusbActivity extends AppCompatActivity {
+public class BusbActivity extends BaseActivity {
+    private static final String TAG = "BusbActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_busb);
+    }
+
+    @Override
+    protected String tag() {
+        return TAG;
     }
 
     public void click(View view) {
